7549bf9adf01d87d5d8495fdeb394d502d54f4e8
WEIJIE CHEN <wchen376@WEIJIEs-MacBook-Pro.local>
2024-05-25 20:58:37 +0800

    try to pull a customized docker

--- monai_train_v2_wb.sh
+++ monai_train_v2_wb.sh
@@ -1,15 +1,15 @@
-docker build -t convez376/monai_with_wb:latest .
-docker run --gpus all -ti -v ./:/wchen376 --ipc=host convez376/monai_with_wb:latest
+# docker build -t convez376/monai_with_wb:latest .
+# docker run --gpus all -ti -v ./:/wchen376 --ipc=host convez376/monai_with_wb:latest
 pip list
-cd /wchen376
-unzip ALTS.zip
-unzip WORD.zip
-rm WORD.zip
-rm ALTS.zip
-mv WORD ./ALTS/data_dir/
-cd ALTS
-python monai_train_v2_wb.py
-rm -rf ./data_dir/WORD
-cd ../
-tar -czvf proj.tar.gz ALTS/proj_dir/WORD_base
-rm -rf ALTS
+# cd /wchen376
+# unzip ALTS.zip
+# unzip WORD.zip
+# rm WORD.zip
+# rm ALTS.zip
+# mv WORD ./ALTS/data_dir/
+# cd ALTS
+# python monai_train_v2_wb.py
+# rm -rf ./data_dir/WORD
+# cd ../
+# tar -czvf proj.tar.gz ALTS/proj_dir/WORD_base
+# rm -rf ALTS
