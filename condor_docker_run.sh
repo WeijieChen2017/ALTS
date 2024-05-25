a6984a1751336ae87e3d286a288dabd3014af23d
WEIJIE CHEN <wchen376@WEIJIEs-MacBook-Pro.local>
2024-05-25 13:36:05 +0800

    try interactive docker building process

--- condor_docker_run.sh
+++ condor_docker_run.sh
@@ -2,5 +2,5 @@ docker build -t convez376/monai_with_wb:latest .
 docker run --gpus all -ti -v ./:/wchen376 --ipc=host convez376/monai_with_wb:latest
 pip list
 unzip ALTS.zip
-cd ALTS.zip
+cd ALTS
 ls
